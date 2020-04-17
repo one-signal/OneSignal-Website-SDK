c51d366741fe8d700cf736bfee99252634bf226a
Rodrigo <rogomezpalacio@protonmail.com>
2020-04-17 15:00:43 -0500

    setTimeout logic nits, setTimeout promise helper, public documentation

---
+++ src/helpers/page/WrapSetTimeoutInPromise.ts
@@ -0,0 +1,14 @@
+
+export async function wrapSetTimeoutInPromise(callback: Function, timeDelaySeconds: number): Promise<void> {
+    await new Promise((resolve, reject) => {
+    setTimeout(async () => {
+        try {
+        await callback();
+        resolve();
+        } catch(e) {
+        reject(e);
+        }
+        }, timeDelaySeconds*1000
+    );
+    });
+}
