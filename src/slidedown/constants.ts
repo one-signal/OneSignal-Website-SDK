4fec7d17969105f3d0c1e3b96156fc55f55023dd
Iryna Trush <iryna@onesignal.com>
2020-07-09 23:18:25 -0700

    Creating constants with css classes and ids
    To be used in slidedown and tagging container

---
+++ src/slidedown/constants.ts
@@ -0,0 +1,37 @@
+export const SlidedownCssClasses = {
+  allowButton: "onesignal-slidedown-allow-button",
+  body: "slidedown-body",
+  buttonIndicatorHolder: "onesignal-button-indicator-holder",
+  cancelButton: "onesignal-slidedown-cancel-button",
+  container: "onesignal-slidedown-container",
+  dialog: "onesignal-slidedown-dialog",
+  footer: "slidedown-footer",
+  reset: "onesignal-reset",
+  savingStateButton: "onesignal-saving-state-button"
+}
+
+export const SlidedownCssIds = {
+  allowButton: "onesignal-slidedown-allow-button",
+  body: "slidedown-body",
+  buttonIndicatorHolder: "onesignal-button-indicator-holder",
+  cancelButton: "onesignal-slidedown-cancel-button",
+  container: "onesignal-slidedown-container",
+  dialog: "onesignal-slidedown-dialog",
+  footer: "slidedown-footer",
+}
+
+export const TaggingContainerCssClasses = {
+  categoryLabelInput: "onesignal-category-label-input",
+  categoryLabelText: "onesignal-category-label-text",
+  categoryLabel: "onesignal-category-label",
+  checkmark: "onesignal-checkmark",
+  taggingContainer: "tagging-container",
+  taggingContainerCol: "tagging-container-col",
+  loadingContainer: "onesignal-loading-container",
+  loadingMessage: "onesignal-loading-message"
+}
+
+export const TaggingContainerCssIds = {
+  loadingContainer: "onesignal-loading-container",
+}
+
