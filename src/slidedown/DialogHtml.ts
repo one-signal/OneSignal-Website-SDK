4796077d9cbf30ad48febbac637a241b08c2845c
Rodrigo <rogomezpalacio@protonmail.com>
2020-07-10 18:01:41 -0500

    fixup! Moved dialog html into a separate file

--- src/slidedown/DialogHtml.ts
+++ src/slidedown/DialogHtml.ts
@@ -7,20 +7,22 @@ export function getDialogHtml(dialogProps: DialogProps): string {
     const finalIcon = icon === 'default-icon' ? DEFAULT_ICON : icon;
     const finalIconClass = icon === 'default-icon' ? 'default-icon' : '';
 
-    return `
-        <div id="normal-slidedown">
-            <div class="slidedown-body" id="slidedown-body">
-                <div class="slidedown-body-icon">
-                    <img alt="notification icon" class="${finalIconClass}" src="${finalIcon}" />
-                </div>
-                <div class="slidedown-body-message">${messageText}</div>
-                <div class="clearfix"/>
-                <div id="onesignal-loading-container"/>
-            </div>
-            <div class="slidedown-footer" id="slidedown-footer">
-                <button id="onesignal-slidedown-allow-button" class="align-right primary slidedown-button">${positiveButtonText}</button>
-                <button id="onesignal-slidedown-cancel-button" class="align-right secondary slidedown-button">${negativeButtonText}</button>
-                <div class="clearfix"/>
-            </div>
-        </div>`;
+    return `` +
+        `<div id="normal-slidedown">` +
+            `<div class="slidedown-body" id="slidedown-body">` +
+                `<div class="slidedown-body-icon">` +
+                    `<img alt="notification icon" class="${finalIconClass}" src="${finalIcon}" />` +
+                `</div>` +
+                `<div class="slidedown-body-message">${messageText}</div>` +
+                `<div class="clearfix"></div>` +
+                `<div id="onesignal-loading-container"></div>` +
+            `</div>` +
+            `<div class="slidedown-footer" id="slidedown-footer">` +
+                `<button id="onesignal-slidedown-allow-button" ` +
+                    `class="align-right primary slidedown-button">${positiveButtonText}</button>` +
+                `<button id="onesignal-slidedown-cancel-button" ` +
+                    `class="align-right secondary slidedown-button">${negativeButtonText}</button>` +
+                `<div class="clearfix"></div>` +
+            `</div>` +
+        `</div>`;
 }
