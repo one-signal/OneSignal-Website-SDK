d79095db8fcd99e528fbccde2b28084b853908e3
Rodrigo <rogomezpalacio@protonmail.com>
2020-07-13 13:05:15 -0500

    fixup! Tagging container implementation

--- src/slidedown/TaggingContainer.ts
+++ src/slidedown/TaggingContainer.ts
@@ -8,7 +8,13 @@ import {
 } from '../utils';
 import { getLoadingIndicatorWithColor } from './LoadingIndicator';
 import TagUtils from '../utils/TagUtils';
-import { SlidedownCssIds, TaggingContainerCssClasses, TaggingContainerCssIds } from './constants';
+import {
+    SlidedownCssIds,
+    TaggingContainerCssClasses,
+    TaggingContainerCssIds,
+    TaggingContainerStrings,
+    COLORS
+} from './constants';
 
 export default class TaggingContainer {
     private html: string = "";
@@ -34,9 +40,9 @@ export default class TaggingContainer {
     public load(): void {
         const loadingContainer = getDomElementOrStub(`#${TaggingContainerCssIds.loadingContainer}`);
         addCssClass(loadingContainer, `${TaggingContainerCssClasses.loadingContainer}`);
-        addDomElement(loadingContainer, 'beforeend', getLoadingIndicatorWithColor('#95A1AC'));
-        addDomElement(loadingContainer, 'beforeend',
-            `<div class="${TaggingContainerCssClasses.loadingMessage}">Fetching your preferences</div>`);
+        addDomElement(loadingContainer, 'beforeend', getLoadingIndicatorWithColor(COLORS.greyLoadingIndicator));
+        addDomElement(loadingContainer, 'beforeend', `<div class="${TaggingContainerCssClasses.loadingMessage}">` +
+            `${TaggingContainerStrings.fetchingPreferences}</div>`);
 
         const allowButton = getDomElementOrStub(`#${SlidedownCssIds.allowButton}`) as HTMLButtonElement;
         allowButton.disabled = true;
